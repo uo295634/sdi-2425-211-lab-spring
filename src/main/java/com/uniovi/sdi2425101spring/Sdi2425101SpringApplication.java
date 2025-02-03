593e915b9155d7eddce1e6966db64a50bec687d5
Miguel <Miguel@DESKTOP-6P6KP15>
2025-02-03 14:02:38 +0100

    SDI-2425-211-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi2425101spring/Sdi2425101SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2425101spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2425101SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2425101SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi2425101spring/Sdi2425101SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2425101spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2425101SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
