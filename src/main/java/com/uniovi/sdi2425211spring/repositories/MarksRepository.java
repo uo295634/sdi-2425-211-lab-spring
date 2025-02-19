6fe30a0c7444a16f475ea6f7c671a4e967c39a07
Miguel <Miguel@DESKTOP-6P6KP15>
2025-02-19 23:06:02 +0100

    SDI-2425-211-4.5-SpringBoot-Búsqueda.

--- src/main/java/com/uniovi/sdi2425211spring/repositories/MarksRepository.java
+++ src/main/java/com/uniovi/sdi2425211spring/repositories/MarksRepository.java
@@ -15,4 +15,10 @@ public interface MarksRepository extends CrudRepository<Mark, Long> {
     @Transactional
     @Query("UPDATE Mark SET resend = ?1 WHERE id = ?2")
     void updateResend(Boolean resend, Long id);
+
+    @Query("SELECT r FROM Mark r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER(r.user.name) LIKE LOWER(?1))")
+    List<Mark> searchByDescriptionAndName(String searchText);
+
+    @Query("SELECT r FROM Mark r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER(r.user.name) LIKE LOWER(?1)) AND r.user = ?2")
+    List<Mark> searchByDescriptionNameAndUser(String searchName, User user);
 }
--- src/main/java/com/uniovi/sdi2425211spring/services/MarksService.java
+++ src/main/java/com/uniovi/sdi2425211spring/services/MarksService.java
@@ -59,5 +59,16 @@ public class MarksService {
         return marks;
     }
 
+    public List<Mark> searchMarksByDescriptionAndNameForUser(String searchText, User user) {
+        List<Mark> marks = new ArrayList<>();
+        searchText = "%"+searchText+"%";
+        if (user.getRole().equals("ROLE_STUDENT")) {
+            marks = marksRepository.searchByDescriptionNameAndUser(searchText, user);
+        }
+        if (user.getRole().equals("ROLE_PROFESSOR")) {
+            marks = marksRepository.searchByDescriptionAndName(searchText);
+        }
+        return marks;
+    }
 
 }
