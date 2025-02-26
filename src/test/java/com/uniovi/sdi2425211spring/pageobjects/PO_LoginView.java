3f9f0311c36a450686f497fdfbac962b371ae1e4
Miguel <Miguel@DESKTOP-6P6KP15>
2025-02-26 22:16:55 +0100

    SDI-2425-211-5.6-Casos Uso VistaLogin.

---
+++ src/test/java/com/uniovi/sdi2425211spring/pageobjects/PO_LoginView.java
@@ -0,0 +1,27 @@
+package com.uniovi.sdi2425211spring.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class PO_LoginView extends PO_NavView{
+    static public void fillLoginForm(WebDriver driver, String dnip, String passwordp){
+        WebElement dni = driver.findElement(By.name("username"));
+        dni.click();
+        dni.clear();
+        dni.sendKeys(dnip);
+        WebElement password = driver.findElement(By.name("password"));
+        password.click();
+        password.clear();
+        password.sendKeys(passwordp);
+        By boton = By.className("btn");
+        driver.findElement(boton).click();
+    }
+
+    static public void clickLogout(WebDriver driver) {
+        By boton = By.xpath("//a[@href='/logout']");
+        driver.findElement(boton).click();
+    }
+
+
+}
